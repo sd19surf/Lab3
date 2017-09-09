e580708fda62bf378d6a67f87140f95fde7a33b7
Shawn <sd19surf@gmail.com>
2017-09-09 17:40:51 -0400

    added a little CSS and fixed table 3

--- index.php
+++ index.php
@@ -11,10 +11,34 @@
 
 
 </head>
+<!-- a little css to pretty things up -->
+<style>
+#main {
+   margin: 10%;
+   display: inline-block;
+   vertical-align: top;
+}
+#table1 {
+   display: inline-block;
+   vertical-align: top;
+   background-color: gray;
+   text-align: center;
+   width: 100%;
+}
+table {
+    align: center;
+    width: 100%;
+}
+#table3 {
+    align: center;
+    width: 30%
+}
 
+</style>
+<div id="main">
 <body>
 <h1>Lab 3 with 3 Tables</h1>
-<h2> I believe I took a couple of varibles used to make the loops and added them together at the time of the cell printed</h2>
+<h2> Three tables one with Calculations for geometric figures. One with a quote that has some adjustments. The last one is the arangement of numbers using loops.</h2>
 <!-- First set everything that will show me all the variables displayed-->
 <?php
 
@@ -50,34 +74,14 @@ $squarePerimeter = round(($squareLength * 4),2); // length * 4
 
 // Second Table information variable set up
 
-$originalQuote = "
-        From childhood's hour I have not been
-        As others were; I have not seen
-        As others saw; I could not bring
-        My passions from a common spring.
-        From the same source I have not taken
-        My sorrow; I could not awaken
-        My heart to joy at the same tone;
-        And all I loved, I loved alone.
-        Then- in my childhood, in the dawn
-        Of a most stormy life- was drawn
-        From every depth of good and ill
-        The mystery which binds me still:
-        From the torrent, or the fountain,
-        From the red cliff of the mountain,
-        From the sun that round me rolled
-        In its autumn tint of gold,
-        From the lightning in the sky
-        As it passed me flying by,
-        From the thunder and the storm,
-        And the cloud that took the form
-        (When the rest of Heaven was blue)
-        Of a demon in my view.";
-$replacedQuote = str_replace("we","me",$originalQuote);
+$originalQuote = "This is our world now... the world of the electron and the switch, the beauty of the baud. We make use of a service already existing without paying for what could be dirt-cheap if it wasn't run by profiteering gluttons, and you call us criminals. We explore... and you call us criminals. We seek after knowledge... and you call us criminals. We exist without skin color, without nationality, without religious bias... and you call us criminals. You build atomic bombs, you wage wars, you murder, cheat, and lie to us and try to make us believe it's for our own good, yet we're the criminals. Yes, I am a criminal. My crime is that of curiosity. My crime is that of judging people by what they say and think, not what they look like. My crime is that of outsmarting you, something that you will never forgive me for.";
+$replaceWords = array("we","We","WE");
+$replacedQuote = str_replace($replaceWords,"Me",$originalQuote);
 $positionQuote[] = array();
-$positionQuote = getAllPositions("m",$originalQuote); //array of all the positions
+$positionQuote = getAllPositions("t",$originalQuote); //array of all the positions
 ?>
 
+<div id="table1">
 <table border="1" padding=".5">
   <tr>
 	<td align="center"><strong>Circle</strong><br>
@@ -104,25 +108,29 @@ $positionQuote = getAllPositions("m",$originalQuote); //array of all the positio
 	</td>
     </tr>
 </table>
+</div>
 <!-- table number 2 quotes and changes -->
 <br>
+<div id="table2">
 <table border="1" padding="0.5">
  <tr>
    <td><center><strong>Original</strong></center><?php print $originalQuote;?></td>
    <td><center><strong>Replaced "we" with "me"</strong></center><?php print $replacedQuote;?></td>
  </tr>
  <tr>
-   <td><center><strong>The position of 'm'</strong></center><?php foreach($positionQuote as $value){
-	 echo "The letter 'm' was found at position: $value <br>";
+   <td><center><strong>The position of 't'</strong></center><?php foreach($positionQuote as $value){
+	 echo "The letter 't' was found at position: $value <br>";
 	}
        ?>
    </td>
    <td><center><strong>Upper Case Original</strong></center><?php echo strtoupper($originalQuote);?></td>
  </tr>
 </table>
+</div>
+<div id="table3">
 <table>
    <tr>
-     <td><strong>Number Increasing</strong><br>
+     <td><strong>Increasing from the left</strong><br>
 	<?php 
 	   $str="";
 	   for($i=1;$i<9;$i++){
@@ -130,7 +138,7 @@ $positionQuote = getAllPositions("m",$originalQuote); //array of all the positio
 		echo "$str <br>";
 	    }?>
      </td>
-     <td align="left"><br><strong>Number Reduction</strong><br>
+     <td align="left"><br><strong>Reducing from the left</strong><br>
 	<?php 	   
 	   $x = 9;
 	   $y = 9;
@@ -145,15 +153,15 @@ $positionQuote = getAllPositions("m",$originalQuote); //array of all the positio
      </td>
    </tr>
    <tr>
-     <td align="right"><strong>Number Increasing</strong><br>
+     <td align="right"><strong>Increasing from the right</strong><br>
 	<?php 
 	   $str="";
 	   for($i=1;$i<9;$i++){
-             $str = $str.$i."  ";
+             $str = "  ".$i.$str;
 		echo "$str <br>";
 	    }?>
      </td>
-     <td align="right"><br><strong>Number Reduction</strong><br>
+     <td align="right"><br><strong>Reducing from the right</strong><br>
 	<?php 	   
 	   $x = 9;
 	   $y = 9;
@@ -168,7 +176,8 @@ $positionQuote = getAllPositions("m",$originalQuote); //array of all the positio
      </td>
    </tr>
 </table>
-
+</div>
+</div>
 
 
 
